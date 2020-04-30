0359fbe5c1764ef8fb2df514d9c88676a95449b1
zlkca <likzhang@gmail.com>
2020-04-30 16:44:22 -0400

    refactory login function

--- models/account.ts
+++ models/account.ts
@@ -7,7 +7,11 @@ import { Config } from "../config";
 import { Utils } from "../utils";
 import moment from 'moment';
 import { EventLog } from "./event-log";
-import { ObjectID } from "mongodb";
+
+import path from 'path';
+import { getLogger } from '../lib/logger'
+const logger = getLogger(path.basename(__filename));
+
 
 const saltRounds = 10;
 
@@ -90,6 +94,41 @@ export class Account extends Model {
     this.cfg = new Config();// JSON.parse(fs.readFileSync('../duocun.cfg.json', 'utf-8'));
     this.twilioClient = require('twilio')(this.cfg.TWILIO.SID, this.cfg.TWILIO.TOKEN);
     this.utils = new Utils();
+  }
+
+
+  // --------------------------------------------------------------------------------------------------
+  // wechat, google or facebook can not use this request to login
+  // username --- optional, can be null, unique  username
+  // password --- mandadory field
+  async doLogin(username: string, password: string) {
+    if (username) {
+      const account: IAccount = await this.findOne({ username });
+      if (account && account.password) {
+        try {
+          bcrypt.compare(password, account.password, (err, matched) => {
+            if (matched) {
+              account.password = '';
+              const cfg = new Config();
+              return jwt.sign(account._id.toString(), cfg.JWT.SECRET); // SHA256
+            } else {
+              logger.error(`login error: ${err}`);
+              return;
+            }
+          });
+
+        } catch (e) {
+          logger.error(`login sign token exception ${e}`);
+          return;
+        }
+      } else {
+        logger.error(`login error: cannot find the user: ${username}`);
+        return;
+      }
+    } else {
+      logger.error(`login error: username is empty`);
+      return;
+    }
   }
 
   jwtSign(accountId: string) {
@@ -542,39 +581,6 @@ export class Account extends Model {
     });
   }
 
-  // --------------------------------------------------------------------------------------------------
-  // wechat, google or facebook can not use this request to login
-  // username --- optional, can be null, unique  username
-  // password --- mandadory field
-  doLogin(username: string, password: string): Promise<string> {
-    return new Promise((resolve, reject) => {
-      let query = null;
-      if (username) {
-        query = { username: username };
-      }
-
-      if (query) {
-        this.findOne(query).then((r: IAccount) => {
-          if (r && r.password) {
-            bcrypt.compare(password, r.password, (err, matched) => {
-              if (matched) {
-                r.password = '';
-                const cfg = new Config();
-                const tokenId = jwt.sign(r._id.toString(), cfg.JWT.SECRET); // SHA256
-                resolve(tokenId);
-              } else {
-                resolve();
-              }
-            });
-          } else {
-            return resolve();
-          }
-        });
-      } else {
-        resolve();
-      }
-    });
-  }
 
   // return tokenId
   async wechatLoginByOpenId(accessToken: string, openId: string) {
