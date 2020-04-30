9b8d83355aedaa74f265fe871dab8365192fa408
zlkca <likzhang@gmail.com>
2020-04-30 16:56:48 -0400

    change updateOne to async function and move to model

--- models/order.ts
+++ models/order.ts
@@ -158,24 +158,6 @@ export class Order extends Model {
     this.locationModel = new Location(dbo);
   }
 
-  updateOne(orderId: any, doc: any, options?: any): Promise<any> {
-    let query = { _id: orderId };
-    return new Promise((resolve, reject) => {
-      if (Object.keys(doc).length === 0 && doc.constructor === Object) {
-        resolve();
-      } else {
-        query = this.convertIdFields(query);
-        doc = this.convertIdFields(doc);
-
-        this.getCollection().then((c: Collection) => {
-          c.updateOne(query, { $set: doc }, options, (err, r: any) => {
-            // {n: 1, nModified: 0, ok: 1}
-            resolve(r.result);
-          });
-        });
-      }
-    });
-  }
   async createOne(order: IOrder) {
     const savedOrders: IOrder[] = [];
     const paymentId = new ObjectID().toString();
