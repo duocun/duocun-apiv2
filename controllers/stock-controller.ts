866673d5fb0ca5fc449998cf3136e9963e9b66d4
bravemaster619 <bravemaster619@hotmail.com>
2020-05-11 21:25:43 +0800

    fix(stock): exclude temporary orders from stock count

--- controllers/stock-controller.ts
+++ controllers/stock-controller.ts
@@ -170,7 +170,8 @@ export class StockController extends Controller {
         $nin: [
           OrderStatus.BAD,
           OrderStatus.DELETED,
-          OrderStatus.DONE
+          OrderStatus.DONE,
+          OrderStatus.TEMP
         ] 
       },
       items: {
